1d105f6447195619398b366518533ef14147b41a
Louise <louise@louisefindlay.com>
2021-02-11 20:29:55 +0000

    Grab colour of Deezer artwork

--- routes/deezerRouter.js
+++ routes/deezerRouter.js
@@ -7,13 +7,42 @@ const deezer = new DeezerPublicApi();
 // Deezer Router
 const deezerRouter = express.Router();
 
+// Colour Modules
+const ColorThief = require('color-thief');
+const colorThief = new ColorThief();
+
+const fs = require('fs');
+const request = require('request');
+
+// Download Files
+const download = (url, path, callback) => {
+    request.head(url, (err, res, body) => {
+        request(url)
+            .pipe(fs.createWriteStream(path))
+            .on('close', callback);
+    });
+};
+
 // Root route
 deezerRouter.get("/", function (req, res) {
-    //Get album list for the given artist id
-    deezer.artist.albums('58671252').then(function (result) {
-        console.log(result);
-    });
     res.render("pages/deezer/index");
+});
+
+deezerRouter.get("/album", function (req, res) {
+    deezer.album('86103822').then(function (result) {
+        let artworkurl = result.cover;
+        const path = './public/img/image.png';
+        download(artworkurl, path, () => {
+            let color = colorThief.getColor(path);
+            console.log("RGB Colour is " + color);
+            res.render('pages/deezer/album', {
+                artworkurl: artworkurl,
+                color: color
+            });
+        });
+    }, function (err) {
+        console.error(err);
+    });
 });
 
 module.exports = deezerRouter;
