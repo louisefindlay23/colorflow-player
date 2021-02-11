d5c21145495994176a878837556ee57fd353c21c
Louise <louise@louisefindlay.com>
2021-02-11 23:03:32 +0000

    Changed text colour if album background is dark

--- public/js/script.js
+++ public/js/script.js
@@ -23,3 +23,26 @@ function togglePlay(id) {
         playButton.classList.toggle("la-pause-circle");
     }
 }
+
+function lightOrDark(color) {
+    color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+    r = color[1];
+    g = color[2];
+    b = color[3];
+    hsp = Math.sqrt(
+        0.299 * (r * r) +
+        0.587 * (g * g) +
+        0.114 * (b * b)
+    );
+    if (hsp > 127.5) {
+
+        return 'light';
+    } else {
+
+        return 'dark';
+    }
+}
+
+//if (document.getElementsByTagName("main")[0].style.backgroundColor === "#000") {
+//    document.getElementsByTagName("main h2")[0].style.backgroundColor[0].style.color = "#fff";
+//}
